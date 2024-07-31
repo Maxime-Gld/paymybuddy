e439990f0cf9dae448a0722523e635fd2d3ba031
Maxime-Gld <Maxime.Gdev@hotmail.com>
2024-08-01 00:01:42 +0200

    creation des dto user

---
+++ src/main/java/com/maxgld/paymybuddy/dto/UserCreateDto.java
@@ -0,0 +1,15 @@
+package com.maxgld.paymybuddy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCreateDto {
+
+    private String username;
+    private String email;
+    private String password;
+}
